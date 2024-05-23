1b8c6575d3d5ce1e2cd6255f453cf262912a303a
foxisobese <mjh03040513@snu.ac.kr>
2024-05-23 23:07:55 +0900

    remove fuse

--- config/loveda/unetformer.py
+++ config/loveda/unetformer.py
@@ -1,7 +1,7 @@
 from torch.utils.data import DataLoader
 from geoseg.losses import *
 from geoseg.datasets.loveda_dataset import *
-from geoseg.models.UNetFormer import UNetFormer, QuantizedUNetFormer, fuse_model, calibrate
+from geoseg.models.UNetFormer import UNetFormer, QuantizedUNetFormer, calibrate
 from catalyst.contrib.nn import Lookahead
 from catalyst import utils
 import torch.quantization as quantization
@@ -34,9 +34,9 @@ monitor_mode = 'max'
 save_top_k = 1
 save_last = True
 check_val_every_n_epoch = 1
-pretrained_ckpt_path = None # the path for the pretrained model weight
-gpus = 'auto'  # default or gpu ids:[0] or gpu nums: 2, more setting can refer to pytorch_lightning
-resume_ckpt_path = None  # whether continue training with the checkpoint, default None
+pretrained_ckpt_path = None
+gpus = 'auto'
+resume_ckpt_path = None
 
 #  define the network, loss + dataloader
 net = QuantizedUNetFormer(num_classes=num_classes)
@@ -51,7 +51,6 @@ def get_training_transform():
     ]
     return albu.Compose(train_transform)
 
-
 def train_aug(img, mask):
     crop_aug = Compose([RandomScale(scale_list=[0.75, 1.0, 1.25, 1.5], mode='value'),
                         SmartCropV1(crop_size=512, max_ratio=0.75, ignore_index=ignore_index, nopad=False)])
@@ -61,29 +60,17 @@ def train_aug(img, mask):
     img, mask = aug['image'], aug['mask']
     return img, mask
 
-
 train_dataset = LoveDATrainDataset(transform=train_aug, data_root='data/LoveDA/Train')
 
 val_dataset = loveda_val_dataset
 
 test_dataset = LoveDATestDataset()
 
-train_loader = DataLoader(dataset=train_dataset,
-                          batch_size=train_batch_size,
-                          num_workers=4,
-                          pin_memory=True,
-                          shuffle=True,
-                          drop_last=True)
+train_loader = DataLoader(dataset=train_dataset, batch_size=train_batch_size, num_workers=4, pin_memory=True, shuffle=True, drop_last=True)
 
-val_loader = DataLoader(dataset=val_dataset,
-                        batch_size=val_batch_size,
-                        num_workers=4,
-                        shuffle=False,
-                        pin_memory=True,
-                        drop_last=False)
+val_loader = DataLoader(dataset=val_dataset, batch_size=val_batch_size, num_workers=4, shuffle=False, pin_memory=True, drop_last=False)
 
-# Quantization
-fuse_model(net)
+# Quantization steps (without fusion)
 net.qconfig = torch.quantization.get_default_qconfig('fbgemm')
 torch.quantization.prepare(net, inplace=True)
 
@@ -93,14 +80,14 @@ calibrate(net, train_loader)
 # Convert to a quantized model
 torch.quantization.convert(net, inplace=True)
 
-# define the optimizer
+# Define the optimizer
 layerwise_params = {"backbone.*": dict(lr=backbone_lr, weight_decay=backbone_weight_decay)}
 net_params = utils.process_model_params(net, layerwise_params=layerwise_params)
 base_optimizer = torch.optim.AdamW(net_params, lr=lr, weight_decay=weight_decay)
 optimizer = Lookahead(base_optimizer)
 lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=max_epoch, eta_min=1e-6)
 
-# Training loop, evaluation, and checkpointing (PLZ make this work)
+# Training loop, evaluation, and checkpointing
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 net.to(device)
 
